b1f36b9b95438fe9a1e1acf937d179bb4abca9c6
baloran <contact@baloran.fr>
2014-06-01 15:56:01 +0200

    intial commit

---
+++ jquery.MyUi.js
@@ -0,0 +1,41 @@
+(function() {
+  var $;
+
+  $ = jQuery;
+
+  $.fn.extend({
+    myui: function(options) {
+      var settings, style;
+      settings = {
+        style: '',
+        text_true: 'On',
+        text_false: 'Off'
+      };
+      settings = $.extend(settings, options);
+      style = ['rounded', 'square'];
+      return this.each(function() {
+        var $input;
+        if (options.style === 'rounded') {
+          $(this).addClass('rounded');
+          $input = $(this).find('input');
+          $input.hide();
+          $input.after('<span class="round"><span></span></span>');
+        }
+        if (options.style === 'square') {
+          $(this).addClass('button');
+          $input = $(this).find('input');
+          $input.hide();
+          $input.wrap('<span class="square">');
+          $input.after('<span class="yes">' + options.text_true + '</span><span class="no">' + options.text_false + '</span>');
+        }
+        if (options.style === 'cross') {
+          $(this).addClass('cross');
+          $input = $(this).find('input');
+          $input.hide();
+          return $input.after('<span class="croix"><span></span></span>');
+        }
+      });
+    }
+  });
+
+}).call(this);
